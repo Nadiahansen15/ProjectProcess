1d1819f3cf296c47ea8a1666609faf01ab3ebf5c
Nadia Hansen <nadiahansen.kea@gmail.com>
2020-02-07 11:20:32 +0100

    program

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Communication communication = new Communication();
+        Planning planning = new Planning();
+        Modeling modeling = new Modeling();
+        Contruction contruction = new Contruction();
+        Deployment deployment = new Deployment();
+
+
+
+    }
+}
